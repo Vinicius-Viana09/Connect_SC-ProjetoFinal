62a9fb5f9e14612bccbaa4f4cff13971e5cf453f
Vinicius Feliciano Viana <49200278850@info.scs>
2022-04-13 11:12:18 -0300

    listagem feita

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,11 @@ import axios from 'axios';
 
 import icone from '../../assets/img/icone.svg'
 import user from '../../assets/img/user-icon.svg'
-import exemplo from '../../assets/img/imagem-exemplo.png'
-import direita from '../../assets/img/direita.svg'
-import esquerda from '../../assets/img/esquerda.svg'
 import cadastrar from '../../assets/img/cadastrar.svg'
 import editar from '../../assets/img/editar.svg'
 import excluir from '../../assets/img/remover.svg'
 import cadastrar_resp from '../../assets/img/btn cadastrar resp.svg'
-import { Component } from 'react';
+import { Component, React } from 'react';
 
 export default class CadastrarCampanha extends Component {
   constructor(props) {
@@ -18,133 +15,146 @@ export default class CadastrarCampanha extends Component {
     this.state = {
       listaCampanha: [],
       usuario: '',
-      campanha: '',
+      nomeCampanha: '',
       dataInicio: new Date(),
       dataFim: new Date(),
       arquivo: ''
     }
   };
-  
+
   buscarCampanhas = () => {
     fetch('http://localhost:5000/api/Campanhas/ListarTodos')
 
-    .then(resposta => resposta.json())
-    
-    .then(dados => this.setState({ listaUsuario: dados }))
+      .then(resposta => resposta.json())
+
+      .then(dados => this.setState({ listaCampanha: dados }))
   };
+
+  // cadastrarCampanha = () => {
+  //   this.setState({ isLoading: true });
+
+  //   let cadastro = {
+  //     nomeCampanha: this.state.nomeCampanha,
+  //     usuario: this.state.usuario,
+  //     dataInicio: this.state.dataInicio,
+  //     dataFim: this.state.dataFim,
+  //     descricao: this.state.descricaoCampanha,
+  //     image: 
+  //   }
+
+    
+  // }
+
   
-  componentDidMount(){
+
+  componentDidMount() {
     this.buscarCampanhas()
   }
-  
-  
+
   render() {
     return (
       <div className="App">
-      <header>
-        <div className="container container_header">
-          <div className="box_header">
-            <h1>Configurando Painel</h1>
-            <img className="icon_header" src={icone} alt="icone" />
+        <header>
+          <div className="container container_header">
+            <div className="box_header">
+              <h1>Configurando Painel</h1>
+              <img className="icon_header" src={icone} alt="icone" />
+            </div>
+            <img className="icon_header" src={user} alt="icone usuário" />
           </div>
-          <img className="icon_header" src={user} alt="icone usuário" />
-        </div>
-      </header>
+        </header>
 
-      <main>
+        <main>
 
-        <section className="section_1">
-          <div className="container_section">
-            <h3>Cadastrar Campanha</h3>
-            <div className="box_cadastrar">
-              <h2 className="h2_cadastrar" >Cadastrar</h2>
-              <form>
-                <form onSubmit={this.cadastrarCampanha}>
-                  <div className="box_form"
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      width: '20vw',
-                    }}
+          <section className="section_1">
+            <div className="container_section">
+              <h3>Cadastrar Campanha</h3>
+              <div className="box_cadastrar">
+                <h2 className="h2_cadastrar" >Cadastrar</h2>
+                <form>
+                  <form onSubmit={this.cadastrarCampanha}>
+                    <div className="box_form"
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '20vw',
+                      }}
                     >
-                    <input type="text"
-                      placeholder="Nome da Campanha:"
+                      <input type="text"
+                        placeholder="Nome da Campanha:"
                       />
-                    <input type="date"
-                      placeholder="Data de Início:"
+                      <input type="date"
+                        placeholder="Data de Início:"
                       />
-                    <input type="date"
-                      placeholder="Data de Expiração"
+                      <input type="date"
+                        placeholder="Data de Expiração"
                       />
-                    <input className="input_file" type="file"
-                    />
-                    <button className="cadastrar_campanha">
-                      <img className="img_cadastrar" src={cadastrar} alt="botão cadastrar" />
-                    </button>
-                    <button className="cadastrar_campanha_2">
-                      <img className="img_cadastrar" src={cadastrar_resp} alt="botão cadastrar" />
-                    </button>
-                  </div>
+                      <div>
+                        <label for="arquivo">Escolher Arquivo</label>
+                        <input type="file" name="arquivo" id="arquivo"/>
+                      </div>
+                      <button className="cadastrar_campanha">
+                        <img className="img_cadastrar" src={cadastrar} alt="botão cadastrar" />
+                      </button>
+                      <button className="cadastrar_campanha_2">
+                        <img className="img_cadastrar" src={cadastrar_resp} alt="botão cadastrar" />
+                      </button>
+                    </div>
+                  </form>
                 </form>
-              </form>
+              </div>
+
             </div>
+          </section>
 
-
-            <div className="box_preview">
-              <img className="img_exemplo" src={exemplo} alt="imagem exemplo" />
-              <div className="controle_preview">
-                <img className="botao_controle" src={esquerda} alt="botão esquerda" />
-                <img className="botao_controle" src={direita} alt="botão direita" />
+          <section className="section_2">
+            <div className="container_section_2">
+              <div className="box_registro">
+                <h2 className="h2_registro" >Registros</h2>
+                <div className="container_table">
+                  <table>
+                    <div className="box_table">
+                      <thead className="box_thead">
+                        <tr >
+                          <th>Nome da Campanha</th>
+                          <th>Usuário</th>
+                          <th>Data de Publicação</th>
+                          <th>Data de Expiração</th>
+                          <th>Ações</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {this.state.listaCampanha.map((campanha) => {
+                          return (
+                            <tr key={campanha.idCampanha}>
+                              <td>{campanha.nomeCampanha}</td>
+                              <td>{campanha.idUsuarioNavigation.nomeUsuario}</td>
+                              <td>{campanha.dataInicio}</td>
+                              <td>{campanha.dataFim}</td>
+                              <td>
+                                <div className='box_botao'>
+                                  <button className='botao_acoes'>
+                                    <img className="img_acoes" src={editar}></img>
+                                  </button>
+                                  <button className='botao_acoes'>
+                                    <img className="img_acoes" src={excluir}></img>
+                                  </button>
+                                </div>
+                              </td>
+                            </tr>
+                          )
+                        })}
+                      </tbody>
+                    </div>
+                  </table>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
-
-        <section className="section_2">
-          <div className="container_section_2">
-            <div className="box_registro">
-              <h2 className="h2_registro" >Registros</h2>
-              <div className="container_table">
-                <table>
-                  <div className="box_table">
-                    <thead className="box_thead">
-                      <tr>
-                        <th>Nome da Campanha</th>
-                        <th>Usuário</th>
-                        <th>Data de Publicação</th>
-                        <th>Data de Expiração</th>
-                        <th>Ações</th>
-                      </tr>
-                    </thead>
-
-                    <tbody className='tb'>
-                      {this.state.listaCampanha.map((campanhas) => {
-                        return (
-                          <tr key={campanhas.idCamapnha}>
-                            <td>{campanhas.nomeCampanha}</td>
-                            <td> teste</td>
-                            <td></td>
-                            <td></td>
-                            <td>
-                              <div className="box_botao">
-                                <button className="botao_acoes"><img className="img_acoes" src={editar} alt="" /></button>
-                                <button className="botao_acoes"><img className="img_acoes" src={excluir} alt="" /></button>
-                              </div>
-                            </td>
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </div>
-                </table>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-    </div>
-  );
-};
+          </section>
+        </main>
+      </div>
+    );
+  };
 }
 
 
