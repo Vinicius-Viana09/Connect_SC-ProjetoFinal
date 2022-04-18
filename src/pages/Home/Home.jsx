37adec6062e8f39a482cc596e66530d56718f711
Vinicius Feliciano Viana <49200278850@info.scs>
2022-04-18 11:10:08 -0300

    funções de cadastro

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+// ESSE É O CERTO!!
+
 import '../../assets/css/Home.css';
 import axios from 'axios';
 
@@ -18,9 +20,14 @@ export default class CadastrarCampanha extends Component {
       nomeCampanha: '',
       dataInicio: new Date(),
       dataFim: new Date(),
+      descricaoCampanha: '',
       arquivo: ''
     }
   };
+
+  atualizaStateCampo = (campo) => {
+    this.setState({ [campo.target.name]: campo.target.value })
+  }
 
   buscarCampanhas = () => {
     fetch('http://localhost:5000/api/Campanhas/ListarTodos')
@@ -30,26 +37,44 @@ export default class CadastrarCampanha extends Component {
       .then(dados => this.setState({ listaCampanha: dados }))
   };
 
-  // cadastrarCampanha = () => {
-  //   this.setState({ isLoading: true });
+  cadastrarCampanha = () => {
+    this.setState({ isLoading: true });
 
-  //   let cadastro = {
-  //     nomeCampanha: this.state.nomeCampanha,
-  //     usuario: this.state.usuario,
-  //     dataInicio: this.state.dataInicio,
-  //     dataFim: this.state.dataFim,
-  //     descricao: this.state.descricaoCampanha,
-  //     image: 
-  //   }
+    let cadastro = {
+      nome: this.state.nomeCampanha,
+      nomeUsuario: this.state.usuario,
+      dataInicio: this.state.dataInicio,
+      dataFim: this.state.dataFim,
+      descricao: this.state.descricaoCampanha,
+      image: this.state.arquivo,
+    };
 
-    
-  // }
+    axios
+      .post('http://localhost:5000/api/Campanhas', cadastro, {
+        headers: {
+          Authorization: 'Bearer' + localStorage.getItem('usuario-login')
+        },
+      })
+      .then((resposta) => {
+        if (resposta.status === 201) {
+          console.log('Campanha cadastrada!');
+          this.setState({ isLoading: false })
+        }
+      })
+      .catch((erro) => {
+        console.log(erro);
+        this.setState({ isLoading: false })
+      })
+      .then(this.buscarCampanhas);
+  }
 
-  
+
 
   componentDidMount() {
     this.buscarCampanhas()
+    this.cadastrarCampanha()
   }
+
 
   render() {
     return (
@@ -71,36 +96,68 @@ export default class CadastrarCampanha extends Component {
               <h3>Cadastrar Campanha</h3>
               <div className="box_cadastrar">
                 <h2 className="h2_cadastrar" >Cadastrar</h2>
-                <form>
-                  <form onSubmit={this.cadastrarCampanha}>
-                    <div className="box_form"
-                      style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        width: '20vw',
-                      }}
-                    >
-                      <input type="text"
-                        placeholder="Nome da Campanha:"
+                <form onSubmit={this.cadastrarCampanha}>
+                  <div className="box_form"
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      width: '20vw',
+                    }}
+                  >
+                    <input
+                      type="text"
+                      placeholder="Nome da Campanha:"
+                      name="nomeCampanha"
+                      value={this.state.nomeCampanha}
+                      onChange={this.atualizaStateCampo}
+                    />
+
+                    <input
+                      type="date"
+                      placeholder="Data de Início:"
+                      name="dataInicio"
+                      value={this.state.dataInicio}
+                      onChange={this.atualizaStateCampo}
+                    />
+
+                    <input
+                      type="date"
+                      placeholder="Data de Expiração"
+                      name="dataFim"
+                      value={this.state.dataFim}
+                      onChange={this.atualizaStateCampo}
+                    />
+
+                    <input
+                      type="text"
+                      placeholder="Descrição"
+                      name="descricao"
+                      value={this.state.descricao}
+                      onChange={this.atualizaStateCampo}
+                    />
+
+                    <div>
+                      <label for="arquivo">Escolher Arquivo</label>
+                      <input
+                        type="file"
+                        name="arquivo"
+                        id="arquivo"
+                        value={this.state.arquivo}
+                        onChange={this.atualizaStateCampo}
                       />
-                      <input type="date"
-                        placeholder="Data de Início:"
-                      />
-                      <input type="date"
-                        placeholder="Data de Expiração"
-                      />
-                      <div>
-                        <label for="arquivo">Escolher Arquivo</label>
-                        <input type="file" name="arquivo" id="arquivo"/>
-                      </div>
-                      <button className="cadastrar_campanha">
-                        <img className="img_cadastrar" src={cadastrar} alt="botão cadastrar" />
-                      </button>
-                      <button className="cadastrar_campanha_2">
-                        <img className="img_cadastrar" src={cadastrar_resp} alt="botão cadastrar" />
-                      </button>
                     </div>
-                  </form>
+
+                    <button className="cadastrar_campanha"
+                      type="submit"
+                      onClick={this.cadastrarCampanha}
+                    >
+                      <img className="img_cadastrar" src={cadastrar} alt="botão cadastrar" />
+                    </button>
+
+                    <button className="cadastrar_campanha_2">
+                      <img className="img_cadastrar" src={cadastrar_resp} alt="botão cadastrar" />
+                    </button>
+                  </div>
                 </form>
               </div>
 
@@ -129,8 +186,12 @@ export default class CadastrarCampanha extends Component {
                             <tr key={campanha.idCampanha}>
                               <td>{campanha.nomeCampanha}</td>
                               <td>{campanha.idUsuarioNavigation.nomeUsuario}</td>
-                              <td>{campanha.dataInicio}</td>
-                              <td>{campanha.dataFim}</td>
+                              <td>{Intl.DateTimeFormat("pt-BR", {
+                                year: "numeric", month: "numeric", day: "numeric"
+                              }).format(new Date(campanha.dataInicio))}</td>
+                              <td>{Intl.DateTimeFormat("pt-BR", {
+                                year: "numeric", month: "numeric", day: "numeric"
+                              }).format(new Date(campanha.dataFim))}</td>
                               <td>
                                 <div className='box_botao'>
                                   <button className='botao_acoes'>
@@ -156,5 +217,3 @@ export default class CadastrarCampanha extends Component {
     );
   };
 }
-
-
